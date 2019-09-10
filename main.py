80c0676e3c554535b5dc67d7e8bf6ed6eb14225b
Ash Dehghan <ashkan@ritual.co>
2019-09-09 23:44:16 -0400

    Final

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-from sklearn.cluster import DBSCAN
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+"""
+	Author: Ash D
+	Date: Sept/2019
+	Description: This is a simple example demonstrating the
+				 unsupervised clustering algorithm DBSCAN.
+				 For more infomration see www.elmspace.com
+"""
+
 import random
-from collections import Counter
+import numpy as np
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+from sklearn.cluster import DBSCAN
 
-def cluster_finder(points):
+
+def cluster_finder(points, eps, min_samples):
+	"""
+	This function will use sklearn DBSCAN and Matplotlib to
+	find clusters in points and plot them.
+	Input:
+		:points: List of x,y coordinates <list of lists>
+		:eps: Minimum distance used by DBSCAN for clusteting <float>
+		:min_samples: Minimum number points required per cluster (used by DBSCAN algo) <int>
+	"""
 	X = np.array(points)
-
-	clustering = DBSCAN(eps=3, min_samples=20).fit(X)
+	clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
 	labels = clustering.labels_
 	labels = [i+1 for i in labels]
-
-	cluster_labels = Counter(labels)
 	colors = cm.rainbow(np.linspace(0, 1, len(set(labels))+1))
-
-	fig = plt.figure(figsize=(10,10))
 	plt.scatter(X[:,0], X[:,1], c=colors[labels])
-	fig.savefig('temp.png', dpi=fig.dpi)
-
+	plt.show()
 
 def create_clusters(nc, npc, bs, delta):
+	"""
+	This function creates a list of [x,y] points based on:
+	Input:
+		:nc: Number of Clusters <int>
+		:npc: Number of points per cluster, <int>
+		:bs: Box size <int>
+		:delta: Size around the center of each cluster point to distribute the
+				cluster points <int>
+	Output:
+		:points: List of x,y coordinates. <list of lists>
+	"""
 	points = []
 	for i in range(nc):
 		x = random.randint(0,bs)
@@ -31,14 +52,12 @@ def create_clusters(nc, npc, bs, delta):
 			points.append([x+delta_x, y+delta_y])
 	for i in range(nc * 100):
 		points.append([random.randint(0,bs), random.randint(0,bs)])
-
 	return points
 
 if __name__ == "__main__":
-
-	nc = 5
-	npc = 50
-	bs = 100
-	delta = 2
+	nc = 8 # Number of clustersc
+	npc = 50 # Number of points per cluster
+	bs = 100 # Box size
+	delta = 2 # Distribution around cluster centers
 	points = create_clusters(nc, npc, bs, delta)
-	cluster_finder(points)
+	cluster_finder(points, 3, 20)
